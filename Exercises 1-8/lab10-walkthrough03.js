8d24b80208cac69ce171939075b972dc951e0b52
Sthankac <65871892+Sthankac@users.noreply.github.com>
2021-02-08 20:04:40 -0500

    1,3 and 6

--- Exercises 1-8/lab10-walkthrough03.js
+++ Exercises 1-8/lab10-walkthrough03.js
@@ -1,7 +1,6 @@
 /* try the different advanced selection calls here */
-$(':password').css('background-color',	'yellow');
-/*----This sets all the password fields to yellow color----*/
-$('form	:nth-child(4n)').css('background-color',	'yellow');
-/*---This selects every 4 child elements in form and changes its bacground color---*/
-$('label:contains("word")').css("color",	"black");
-/*----This changes the label password color to black----*/
+
+//$(':password').css('background-color',	'yellow');
+//
+$('form	:nth-child(4n)').css('background-color', 'yellow');
+$('label:contains("word")').css("color", "black");
