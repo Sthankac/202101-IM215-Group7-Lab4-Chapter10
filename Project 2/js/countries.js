1ff6745b230753cdc51b155b2a74ea94c710760e
Sthankac <65871892+Sthankac@users.noreply.github.com>
2021-02-26 22:37:43 -0500

    Add files via upload

---
+++ Project 2/js/countries.js
@@ -0,0 +1,27 @@
+    var countries = [
+    {
+        id:  "DE",
+        name: "Germany"
+    },
+    {
+        id:  "GR",
+        name: "Greece"
+    },
+    {
+        id:  "IT",
+        name: "Italy"
+    },
+    {
+        id:  "CA",
+        name: "Canada"
+    },
+    {
+        id:  "GB",
+        name: "United Kingdom"
+    },
+    {
+        id:  "US",
+        name: "United States"
+    },    
+    
+];
---
+++ Project 2/js/project.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    images.forEach(element => {
+        var image = $("<img></img>").attr('src', 'images/square/' + element.path).attr('alt', element.title);
+        $(".gallery").append(image);
+    });
+    $("img").mouseenter(function(event) {
+        $(this).addClass("gray");
+        var di = $("<div></div>").attr('id', 'preview');
+        var image = $("<img></img>").attr('src', $(this).attr('src').replace('square', 'medium')).attr('alt', $(this).attr('alt'));
+        div.append(image);
+        images.forEach(ele => {
+            if (ele.title == $(this).attr('alt')) {
+                var p = $("<p></p>").html(ele.title + " " + ele.city + ", " + ele.country + " [" + ele.taken + "]");
+                div.append(p);
+            }
+        });
+        $("body").append(di);
+    });
+    $("img").mousemove(function(event) {
+        $("#preview").css('left', event.pageX).css('top', event.pageY);
+    });
+    $("img").mouseleave(function() {
+        $("#preview").remove();
+        $(this).removeClass("gray");
+    });
+});
